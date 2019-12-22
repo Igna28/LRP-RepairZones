18bbf74dfb094d7f834cd17552bf58eb0e5ba403
Igna28 <53914192+Igna28@users.noreply.github.com>
2019-12-22 16:37:40 -0300

    remove debug prints

--- server.lua
+++ server.lua
@@ -9,15 +9,12 @@ end)
 ESX.RegisterServerCallback('LRP-RepairZone:RemoveMoney', function(source, cb, price)
     local xPlayer = ESX.GetPlayerFromId(source)
     if (xPlayer.getMoney() >= price) then
-        print ('->LRP-RepairZones: Retiro de Efectivo: $' .. price .. " - ID " .. source)
         xPlayer.removeMoney(price)
         cb(true)
     elseif (xPlayer.getBank() >= price) then
-        print ('->LRP-RepairZones: Retiro de Banco: $' .. price .. " - ID " .. source)
         xPlayer.removeAccountMoney('bank', price)
         cb(true)
     else
-        print ('->LRP-RepairZones: No tiene dinero suficiente' .. " - ID " .. source)
         cb(false)
     end
 end)
